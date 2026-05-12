9b5c813d2578623860bc68f49c0dba859ecfb99e
watanabe <chacha1104.kanna@gmail.com>
2026-05-12 10:08:18 +0900

    ログイン・ログアウト機能を追加

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,9 @@ CREATE TABLE products (
   price INT          NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS users (
+  id       SERIAL PRIMARY KEY,
+  name     VARCHAR(100) NOT NULL,
+  email    VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+);
